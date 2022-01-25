c5b3abe841ad52d52a4422d3cd5c42ecfc93bde6
Brandon H <hartwebdev92@gmail.com>
2022-01-25 11:16:42 -0800

    promise practice

--- js/promises.js
+++ js/promises.js
@@ -3,7 +3,7 @@ function calcSqArea()
     return new Promise(function(resolve, reject){
         setTimeout(() => {
             const sqArea = 5 * 5;
-            reject(sqArea);
+            resolve(sqArea);
         })
     })
 }
@@ -16,8 +16,27 @@ function onRejected(reason)
     console.log('rejected: ', reason);
 }
 
-calcSqArea()
+Promise.reject('testresolve')
 .then(onFulfilled)
 .catch((reason) => {
     console.log('catch: ', reason);
 });
+
+let myNum = 5;
+
+function changeNum()
+{
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            myNum = 10;
+            resolve(myNum);
+        }, 500);
+    })
+}
+
+async function startChange()
+{
+    await changeNum();
+    console.log(myNum);
+};
+startChange();
